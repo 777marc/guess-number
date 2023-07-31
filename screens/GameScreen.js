be460c1241430812a2e845c13b7bc7ebcad4d9cb
Marc Mendoza <777marc@gmail.com>
2023-07-31 16:58:09 -0400

    title component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,25 +1,27 @@
 import { StyleSheet, Text, View } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen() {
   return (
     <View style={styles.container}>
-      <Text>Game Screen</Text>
+      <Title>Opponent's Guess</Title>
       {/* // the guess */}
       <View>
         <Text>Higher or Lower?</Text>
         {/* // + - buttons */}
       </View>
-      <View><Text>Round:</Text></View>
+      <View>
+        <Text>Round:</Text>
+      </View>
     </View>
-  )
-
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-  }
-})
+  },
+});
 
 export default GameScreen;

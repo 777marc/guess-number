ca2d90088721b3c38abbadd6a4dbba2960585f74
Marc Mendoza <777marc@gmail.com>
2023-08-07 17:01:24 -0400

    misc cleanup; number container

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (min - max)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState();
+
   return (
     <View style={styles.container}>
       <Title>Opponent's Guess</Title>
